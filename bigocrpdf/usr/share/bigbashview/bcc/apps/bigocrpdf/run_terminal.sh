ae3b104d68734fbed53701b9984e7fd72b4d64d1
Bruno Gonçalves <bigbruno@gmail.com>
2022-02-18 01:01:11 -0300

    Update run_terminal.sh

--- bigocrpdf/usr/share/bigbashview/bcc/apps/bigocrpdf/run_terminal.sh
+++ bigocrpdf/usr/share/bigbashview/bcc/apps/bigocrpdf/run_terminal.sh
@@ -10,7 +10,7 @@ PID="$$" WINDOW_ID="$WINDOW_ID" ./run_terminal_resize.sh &
 OIFS=$IFS
 IFS=$'\n'
 
-    ocrmypdf --force-ocr --threshold -l "$(cat ~/.config/bigocrpdf/lang)" --optimize "$(cat ~/.config/bigocrpdf/quality)" "$(cat ~/.config/bigocrpdf/selected-file)" "$(cat ~/.config/bigocrpdf/savefile)" 
+    ocrmypdf --force-ocr -l "$(cat ~/.config/bigocrpdf/lang)" --optimize "$(cat ~/.config/bigocrpdf/quality)" "$(cat ~/.config/bigocrpdf/selected-file)" "$(cat ~/.config/bigocrpdf/savefile)" 
 
 
 if [ "$(xwininfo -id $WINDOW_ID 2>&1 | grep -i "No such window")" != "" ]; then
