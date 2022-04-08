118090504060946a025173352007208e4b39be39
Bruno Goncalves <bigbruno@gmail.com>
2022-04-08 09:07:51 -0300

    Add support to images

--- bigocrpdf/usr/share/bigbashview/bcc/apps/bigocrpdf/run_terminal.sh
+++ bigocrpdf/usr/share/bigbashview/bcc/apps/bigocrpdf/run_terminal.sh
@@ -7,15 +7,21 @@
 
 PID="$$" MARGIN_TOP_MOVE="-90" WINDOW_HEIGHT=12 WINDOW_ID="$WINDOW_ID" ./run_terminal_resize.sh &
 
+
+if [ "$(grep '.pdf' ~/.config/bigocrpdf/selected-file)" = "" ]; then
+#IMAGES
+    img2pdf $(cat ~/.config/bigocrpdf/selected-file) | ocrmypdf --image-dpi 300 --force-ocr -l "$(cat ~/.config/bigocrpdf/lang)" $(cat ~/.config/bigocrpdf/align) --optimize "$(cat ~/.config/bigocrpdf/quality)" --sidecar "$HOME/.config/bigocrpdf/text.txt" - "$(cat ~/.config/bigocrpdf/savefile)"
+else
+# PDF
 OIFS=$IFS
 IFS=$'\n'
+    ocrmypdf --force-ocr -l "$(cat ~/.config/bigocrpdf/lang)" $(cat ~/.config/bigocrpdf/align) --optimize "$(cat ~/.config/bigocrpdf/quality)" --sidecar "$HOME/.config/bigocrpdf/text.txt" "$(cat ~/.config/bigocrpdf/selected-file)" "$(cat ~/.config/bigocrpdf/savefile)"
+fi
 
-    ocrmypdf --force-ocr -l "$(cat ~/.config/bigocrpdf/lang)" --optimize "$(cat ~/.config/bigocrpdf/quality)" "$(cat ~/.config/bigocrpdf/selected-file)" "$(cat ~/.config/bigocrpdf/savefile)" 
 
-
+    
 if [ "$(xwininfo -id $WINDOW_ID 2>&1 | grep -i "No such window")" != "" ]; then
     kill -9 $PID
     exit 0
 fi
 
-IFS=$OIFS
